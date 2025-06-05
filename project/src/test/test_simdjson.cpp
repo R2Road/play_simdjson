1d174ae4f1dd09c5dd52b7749f4e20e885bac77a
R2Road <studior2road@gmail.com>
2025-06-05 18:07:18 +0900

    add test : Basic

--- project/src/test/test_simdjson.cpp
+++ project/src/test/test_simdjson.cpp
@@ -20,7 +20,36 @@ namespace test_simdjson
 		{
 			LS();
 
+			DECLARATION_MAIN( simdjson::dom::parser parser );
+
+			LS();
+
+			DECLARATION_MAIN( const auto abstract_json = R"(
+				[
+					{  "12345" : {"a":12.34, "b":56.78, "c": 9998877}   },
+					{  "12545" : {"a":11.44, "b":12.78, "c": 11111111}  }
+				]
+			)"_padded );
+
+			LS();
+
 			{
+				OUTPUT_FILE_RANGE( __FILE__, 41, 52 );
+
+				SS();
+
+				for( simdjson::dom::object obj : parser.parse( abstract_json ) )
+				{
+					for( const auto& key_value : obj )
+					{
+						std::cout << "key: " << key_value.key << " : ";
+
+						simdjson::dom::object innerobj = key_value.value;
+						std::cout << "a: " << double( innerobj["a"] ) << ", ";
+						std::cout << "b: " << double( innerobj["b"] ) << ", ";
+						std::cout << "c: " << int64_t( innerobj["c"] ) << std::endl;
+					}
+				}
 			}
 
 			LS();
